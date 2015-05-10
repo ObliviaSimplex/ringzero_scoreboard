8905a623f609d32323602edf935f8ea4ce6355dd
Oblivia Simplex <lucca.fraser@gmail.com>
2015-05-10 16:09:58 -0300

    Added list of all the challenges completed by team

--- scoreboard.py
+++ scoreboard.py
@@ -14,21 +14,55 @@ def getscore(uid):
         if m is not None:
             return m.group(1)
 
+def get_challenge_dict(uid):
+    response = requests.get("http://ringzer0team.com/profile/" + str(uid))
+    doc = response.text
+    found = False
+    challenge_points = {}
+    splitdoc = doc.split('\n')
+    for l in range(len(splitdoc)):
+        line = splitdoc[l]
+        # '\<a href=\"\/challenges\/.*'
+        m = re.match(r'.*challenges.*', line)
+        if m is not None:
+            
+            points = ("".join([c for c in splitdoc[l+3] if c in "0123456789"]))
+            
+            challenge_points[m.group(0)]=points
+    return challenge_points
+    
+        
 def main():
     print """<html>
   <body>
     <h3>Scoreboard</h3>
     <table>
       <tr><th>User</th><th>Points</th></tr>
-"""
+    """
+    challenge_points = {}
     for line in fileinput.input("userids.txt"):
         user = line.strip('\n').split(',')
+        cp = get_challenge_dict(user[1])
+        if len(cp) > 0:
+            challenge_points.update(cp)
 	anchor = "<a href=\"http://ringzer0team.com/profile/" + user[1] \
 		+ "\">" + user[0] + "</a>"
         if user[1] != '':
             print "<tr><td>" + anchor + '</td><td>' + getscore(user[1]) + '</td></tr>'
         else:
             print "<tr><td>" + user[0] + '</td><td>unavailable</td></tr>'
+    
+    print "<tr><td>ALL CHALLENGES COMPLETED</td><td></td></tr>"
+    grand_total=0
+    for c in challenge_points:
+        try:
+            grand_total += int(challenge_points[c])
+            print "<tr><td>"+c+"</td><td>"+challenge_points[c]+"</td></tr>"
+        except:
+            pass
+        
+    print "<tr><td>GRAND TOTAL</td><td>"+str(grand_total)+"</td></tr>"
+    
     print """</table></body></html>"""
 
 main()
